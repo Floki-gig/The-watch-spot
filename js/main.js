b0b379fde0718255060c7f0730fdb0917a7027f8
Floki-gig <alexeyorekhov2005@mail.ru>
2024-04-10 14:19:58 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var gallery = document.querySelectorAll('.materialboxed');
+    var instances_gallery = M.Materialbox.init(gallery);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances_sidenav = M.Sidenav.init(sidenav);
+});
